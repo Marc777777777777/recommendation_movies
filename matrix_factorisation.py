b83b598a1864c97ee49f1b6514b14f7987a67262
Marc777777777777 <m7rc.k7sp7r@gmail.com>
2024-09-25 15:56:23 +0200

    Add files via upload

--- matrix_factorisation.py
+++ matrix_factorisation.py
@@ -3,17 +3,17 @@ import metric
 
 class matrix_factorisation:
     """
-    An object of this class will implement the training and 
+    An object of this class will implement the training and
     """
-    def __init__(self, k, m):
+    def __init__(self, k, m,n):
         """
         The object will have the I matrix and the U matrix.
-        The training will use some methods to 
+        The training will use some methods to
         """
         self.k = k     # TODO: Est ce que k est directement à l'initialisation ?
         self.I = np.zeros((m,self.k))
-        self.U = np.zeros((self.k,m))
-    
+        self.U = np.zeros((n,self.k))
+
     def train_GD(self, R, tau):
         """
         This function will train the model with parameter k using gradient descent (slide 22).
@@ -21,14 +21,17 @@ class matrix_factorisation:
 
         cost_function = np.trace()
         # TODO
-    
+
     def train_ALS(self,R):
         """
         This function will train the model with parameter k using ALS (slide 22).
         """
-        # TODO: ????
+        T = 1000
+        lmbd = 1
+        mu = 1
+        for i in range(T):
+            (self.I, self.U) = (R@ self.U @np.linalg.inv(self.U.T@self.U + lmbd *np.eye(self.k)),R.T@I@np.linalg.inv(self.I.T@self.I+ mu*np.eye(self.k)))
 
-    
     def predict(self, R):
         """
         This function will be used to predict R_hat.
@@ -48,5 +51,5 @@ class matrix_factorisation:
         """
         R_hat = np.array()
         return metric.loss0_1(R, R_hat)
-    
+
 
