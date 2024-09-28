2a4b33bdd9fc6bf6611f6fea89ab5d3af2fda4f4
Rothy <rithysochet77@gmail.com>
2024-09-28 18:47:54 +0200

    Adding some improvements + ALS successful

--- main.py
+++ main.py
@@ -15,15 +15,19 @@ data_genre = np.load("namesngenre.npy")     # shape = (4980, 2)
 
 
 # PREPROCESSING OF THE TRAINING DATA
-data_train = np.nan_to_num(data_train)
+data_train = np.nan_to_num(data_train, nan=np.nanmean(data_train, axis=1, keepdims=True))
+
+#average = np.nanmean(data_train)
+#data_train = np.nan_to_num(data_train, nan=average)
+
 m,n = data_train.shape
-method = MF.matrix_factorisation(k=500, m=m, n=n)
+method = MF.matrix_factorisation(k=1, m=m, n=n)
 
 # TRAINING START
 print("Start training")
 start_time = time.time()
-method.train_GD(data_train, 0.015, 0.015, lamba=0, mu=0,nb_ite=50)
-#method.train_ALS(data_train, lmbd=0.2, mu=0.2)
+#method.train_GD(data_train, 0.001, 0.001, lmbda=0, mu=0, nb_ite=100, alternate_counter=20)
+method.train_ALS(data_train,lmbda=1, mu=1)
 end_time = time.time()
 print("Training time =", end_time-start_time, "s")
 #average = np.nanmean(data_test)
@@ -33,10 +37,9 @@ R_isnan = np.isnan(data_test)       # R_isnan contains an array of shape R.shape
 T = np.argwhere(R_isnan == False)    # T contains a list of list containing the indices where R is NOT NaN
 data_test = np.nan_to_num(data_test)
 
-prediction = method.predict(data_test)
-print(prediction)
+prediction = method.predict(round=True)
 print("Score RMSE =", metric.RMSE(data_test, prediction, T))
-
+print("Score accuracy =", metric.accuracy(data_test, prediction, T))
 #TODO:
 # Step 1: Modify the nan elements of the dataset
 # Step 2: Use an implementation
--- matrix_factorisation.py
+++ matrix_factorisation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import metric
+import matplotlib.pyplot as plt
 
 class matrix_factorisation:
     """
@@ -10,49 +11,86 @@ class matrix_factorisation:
         The object will have the I matrix and the U matrix.
         The training will use some methods to 
         """
-        ratings = np.arange(0.5, 5.5, 0.5)
         self.k = k
-        self.I = np.random.choice(ratings, (m, self.k))
-        self.U = np.random.choice(ratings, (n, self.k))
-        #self.I = np.ones((m, self.k))
-        #self.U = np.ones((n, self.k))
+        #ratings = np.arange(0.5, 5.5, 0.5)
+        #self.I = np.random.choice(ratings, (m, self.k))
+        #self.U = np.random.choice(ratings, (n, self.k))
+        self.I = np.ones((m, self.k))
+        self.U = np.ones((n, self.k))
 
 
-    def train_GD(self, R, tau1, tau2, lamba=0.1, mu=0.1, nb_ite=100):
+    def train_GD(self, R, tau1, tau2, lmbda=0.001, mu=0.001, nb_ite=500, alternate_counter=25):
         """
         This function will train the model with parameter k using gradient descent (slide 22).
         """
-        #cost_function = np.trace(R.T@R) - 2*np.trace(R.T@self.I@self.U.T) + np.trace(self.U@self.I.T@self.I@self.U.T) + lamba*np.trace(self.U.T@self.U) + mu*np.trace(self.I.T@self.I)
+        cost_function = cost(R, self.I, self.U, lmbda=lmbda, mu=mu)
+        cost_val = [cost_function]
         counter = 0
         for ite in range(nb_ite):
             print("ite numero:",ite)
 
-            if counter < 10:
-                C_deriv_I = -2*R@self.U + 2*self.I@self.U.T@self.U + 2*lamba*self.I
-                self.I = self.I - tau1*C_deriv_I
-                counter += 1
-                
-            else:
+            if counter < alternate_counter:
                 C_deriv_U = -2*R.T@self.I + 2*self.U@self.I.T@self.I + 2*mu*self.U
                 self.U = self.U - tau2*C_deriv_U
                 counter += 1
+                
+                
+            else:
+                C_deriv_I = -2*R@self.U + 2*self.I@self.U.T@self.U + 2*lmbda*self.I
+                self.I = self.I - tau1*C_deriv_I
+                counter += 1
 
-                if counter >= 20:
+                if counter >= 2*alternate_counter:
                     counter = 0
         
-    def train_ALS(self, R, lmbd=1., mu=1., nb_ite = 1000):
+            cost_val.append(cost(R, self.I, self.U, lmbda=lmbda, mu=mu))
+            print("Cost =", cost_val[-1])
+            print("Max U =", np.max(self.U))
+            print("Max Grad U =", np.max(C_deriv_U))
+            #print("Max I =", np.max(self.I))
+            #print("Max Grad I =", np.max(C_deriv_I))
+            print()
+
+        #plt.plot(np.arange(0, nb_ite+1, 1), cost_val)
+        #plt.semilogy(np.arange(0, nb_ite+1, 1), cost_val)
+        #plt.show()
+
+    def train_ALS(self, R, lmbda=1., mu=1., nb_ite=200, alternate_counter=25):
         """
         This function will train the model with parameter k using ALS (slide 22).
         """
-        for _ in range(nb_ite):
-            (self.I, self.U) = (R@ self.U @np.linalg.inv(self.U.T@self.U + lmbd *np.eye(self.k)), R.T@self.I@np.linalg.inv(self.I.T@self.I+ mu*np.eye(self.k)))
+        cost_function = cost(R, self.I, self.U, lmbda=lmbda, mu=mu)
+        cost_val = [cost_function]
+        counter = 0
 
+        for ite in range(nb_ite):
+            #(self.I, self.U) = (R@ self.U @np.linalg.inv(self.U.T@self.U + lmbda *np.eye(self.k)), R.T@self.I@np.linalg.inv(self.I.T@self.I+ mu*np.eye(self.k)))
+            print("Iteration:", ite)
+            if counter < alternate_counter:
+                self.U = R.T@self.I@np.linalg.inv(self.I.T@self.I+ mu*np.eye(self.k))
+                counter += 1
+            
+            else:
+                self.I = R@ self.U @np.linalg.inv(self.U.T@self.U + lmbda *np.eye(self.k))
+                counter += 1
+
+                if counter >= 2*alternate_counter:
+                    counter = 0
+
+            cost_val.append(cost(R, self.I, self.U, lmbda=lmbda, mu=mu))
+
+        #plt.plot(np.arange(0, nb_ite+1, 1), cost_val)
+        #plt.semilogy(np.arange(0, nb_ite+1, 1), cost_val)
+        #plt.show()
     
-    def predict(self, R):
+    def predict(self, round=True):
         """
         This function will be used to predict R_hat.
         """
-        return self.I@self.U.T
+        R_hat = self.I@self.U.T
+        if round:
+            R_hat = np.round(2*R_hat)/2
+        return R_hat
 
     def score_RMSE(self, R, T):
         """
@@ -75,3 +113,16 @@ class matrix_factorisation:
         R_hat = self.I@self.U.T
         return metric.accuracy(R, R_hat, T)
 
+def cost(R, I, U, lmbda=1., mu=1.):
+    """
+    This function returns the cost function defined in the slide.
+    """
+    return np.trace(R.T@R) - 2*np.trace(R.T@I@U.T) + np.trace(U@I.T@I@U.T) + lmbda*np.trace(U.T@U) + mu*np.trace(I.T@I)
+
+def roundup_prediction(R_hat):
+    """
+    This function should take a rating matrix that is not exact (the rates are not among the possible one),
+    and it will roundup the elements to the closest correct rate. 
+    """
+    R_hat_roundup = np.zeros(R_hat.shape)
+    return R_hat_roundup
