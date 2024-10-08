fec5cafe9c31570a856c34858329c292a00e6da3
Rothy <rithysochet77@gmail.com>
2024-10-08 21:18:13 +0200

    Adding the report and some plots

--- main.py
+++ main.py
@@ -28,7 +28,8 @@ R_isnan = np.isnan(data_test)        # R_isnan contains an array of shape R.shap
 T_test = np.argwhere(R_isnan == False)    # T contains a list of list containing the indices where R is NOT NaN
 
 # List for plots:
-k_values = [1, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200]
+k_values = [10*i for i in range(25)]
+k_values[0] = 1
 #k_values = [1,1,1,1,1, 20,20,20,20,20, 40,40,40,40,40, 60,60,60,60,60, 80,80,80,80,80, 100,100,100,100,100, 120,120,120,120,120, 140,140,140,140,140, 160,160,160,160,160, 180,180,180,180,180, 200,200,200,200,200]
 lmbda=1
 mu=0.5
@@ -58,31 +59,31 @@ for k in k_values:
     time_values.append(end_time-start_time)
     print("Training time =", time_values[-1], "s")
     
-    uniform_random_RMSE = 0
-    uniform_random_accuracy = 0
-    gaussian_random_RMSE = 0
-    gaussian_random_accuracy = 0
-    for seed in range(5):
-        uniform_random_method = MF.matrix_factorisation(k=k, m=m, n=n, random_uniform=True, seed=seed)
-        uniform_random_method.train_ALS(data_train, lmbda=lmbda, mu=mu)
-        uniform_prediction_random = uniform_random_method.predict(round=True)
-        uniform_random_RMSE += metric.RMSE(data_test, uniform_prediction_random, T_test)
-        uniform_random_accuracy += metric.accuracy(data_test, uniform_prediction_random, T_test)
+    # uniform_random_RMSE = 0
+    # uniform_random_accuracy = 0
+    # gaussian_random_RMSE = 0
+    # gaussian_random_accuracy = 0
+    # for seed in range(5):
+    #     uniform_random_method = MF.matrix_factorisation(k=k, m=m, n=n, random_uniform=True, seed=seed)
+    #     uniform_random_method.train_ALS(data_train, lmbda=lmbda, mu=mu)
+    #     uniform_prediction_random = uniform_random_method.predict(round=True)
+    #     uniform_random_RMSE += metric.RMSE(data_test, uniform_prediction_random, T_test)
+    #     uniform_random_accuracy += metric.accuracy(data_test, uniform_prediction_random, T_test)
 
-        gaussian_random_method = MF.matrix_factorisation(k=k, m=m, n=n, random_gaussian=True, seed=seed)
-        gaussian_random_method.train_ALS(data_train, lmbda=lmbda, mu=mu)
-        gaussian_prediction_random = gaussian_random_method.predict(round=True)
-        gaussian_random_RMSE += metric.RMSE(data_test, gaussian_prediction_random, T_test)
-        gaussian_random_accuracy += metric.accuracy(data_test, gaussian_prediction_random, T_test)
+    #     gaussian_random_method = MF.matrix_factorisation(k=k, m=m, n=n, random_gaussian=True, seed=seed)
+    #     gaussian_random_method.train_ALS(data_train, lmbda=lmbda, mu=mu)
+    #     gaussian_prediction_random = gaussian_random_method.predict(round=True)
+    #     gaussian_random_RMSE += metric.RMSE(data_test, gaussian_prediction_random, T_test)
+    #     gaussian_random_accuracy += metric.accuracy(data_test, gaussian_prediction_random, T_test)
 
-    uniform_RMSE_values_test.append(uniform_random_RMSE/5)
-    uniform_accuracy_values_test.append(uniform_random_accuracy/5)
-    gaussian_RMSE_values_test.append(gaussian_random_RMSE/5)
-    gaussian_accuracy_values_test.append(gaussian_random_accuracy/5)
+    # uniform_RMSE_values_test.append(uniform_random_RMSE/5)
+    # uniform_accuracy_values_test.append(uniform_random_accuracy/5)
+    # gaussian_RMSE_values_test.append(gaussian_random_RMSE/5)
+    # gaussian_accuracy_values_test.append(gaussian_random_accuracy/5)
 
     prediction = method.predict(round=True)
-   #RMSE_values_train.append(metric.RMSE(data_train, prediction, T_train))
-   #accuracy_values_train.append(metric.accuracy(data_train, prediction, T_train))
+    RMSE_values_train.append(metric.RMSE(data_train, prediction, T_train))
+    accuracy_values_train.append(metric.accuracy(data_train, prediction, T_train))
     RMSE_values_test.append(metric.RMSE(data_test, prediction, T_test))
     accuracy_values_test.append(metric.accuracy(data_test, prediction, T_test))
     print("Score RMSE test =", RMSE_values_test[-1])
@@ -103,8 +104,9 @@ for k in k_values:
 #plt.show()
 
 plt.plot(k_values, RMSE_values_test, label="Testing RMSE", color='b')
-plt.plot(k_values, uniform_RMSE_values_test, label="Testing uniform init RMSE", color='g')
-plt.plot(k_values, gaussian_RMSE_values_test, label="Testing gaussian init RMSE", color='r')
+plt.plot(k_values, RMSE_values_train, label="Training RMSE", color='r')
+# plt.plot(k_values, uniform_RMSE_values_test, label="Testing uniform init RMSE", color='g')
+# plt.plot(k_values, gaussian_RMSE_values_test, label="Testing gaussian init RMSE", color='r')
 plt.title("RMSE score depending on k")
 plt.xlabel("k")
 plt.ylabel("RMSE")
@@ -126,8 +128,9 @@ plt.show()
 
 
 plt.plot(k_values, accuracy_values_test, label="Testing accuracy", color='b')
-plt.plot(k_values, uniform_accuracy_values_test, label="Testing uniform init accuracy", color='g')
-plt.plot(k_values, gaussian_accuracy_values_test, label="Testing gaussian init accuracy", color='r')
+plt.plot(k_values, accuracy_values_train, label="Training accuracy", color='r')
+# plt.plot(k_values, uniform_accuracy_values_test, label="Testing uniform init accuracy", color='g')
+# plt.plot(k_values, gaussian_accuracy_values_test, label="Testing gaussian init accuracy", color='r')
 plt.title("Accuracy score depending on k")
 plt.xlabel("k")
 plt.ylabel("accuracy")
